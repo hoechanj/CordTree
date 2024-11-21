4eeee36cc4635663ab3575ef3150d7ca67992f0d
hoechanj <154239980+hoechanj@users.noreply.github.com>
2024-11-21 13:22:23 +0900

    Title: 세 정수형 변수 선언, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241121/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int A = 7;
+        int B = 23;
+        int C = 30;
+        System.out.println(A + " + " + B + " = " + C);
+        }
+}
