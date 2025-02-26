083fae8837c287b2069f0ba5bfdce62987c548ab
hoechanj <154239980+hoechanj@users.noreply.github.com>
2025-02-26 16:35:53 +0900

    Title: 세 실수의 반올림, Time: 133ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 250226/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double c = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.printf("%.3f",a,b,c);
+    }
+}
